d06d094e2a4fd9b6e6064a705e1aec1190e0dbb5
Anna Fritz <anna_eida@gmx.de>
2024-09-03 09:16:10 +0200

    added class DrawableObjects for better structure

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -25,22 +25,22 @@ class MovableObjects extends DrawableObjects {
         return this.y < 155;
     }
 
-    loadImage(path){
-        this.img = new Image();
-        this.img.src = path;
-    }
+    // loadImage(path){
+    //     this.img = new Image();
+    //     this.img.src = path;
+    // }
 
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+    // loadImages(arr) {
+    //     arr.forEach((path) => {
+    //         let img = new Image();
+    //         img.src = path;
+    //         this.imageCache[path] = img;
+    //     });
+    // }
 
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
+    // draw(ctx) {
+    //     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    // }
 
     drawFrame(ctx) {
         if (this instanceof Character || this instanceof Chicken) {
