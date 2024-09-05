53f3cb8bdbbe3db7621c29d7294bd348af5ded20
Anna Fritz <anna_eida@gmx.de>
2024-09-05 12:10:57 +0200

    change colours of statusbars

--- models/statusbar-endboss.class.js
+++ models/statusbar-endboss.class.js
@@ -6,12 +6,12 @@ class StatusBarEndboss extends DrawableObjects {
     percentage = 100;
 
     IMAGES_ENDBOSS = [
-        '../img/7_statusbars/2_statusbar_endboss/blue/blue0.png',
-        '../img/7_statusbars/2_statusbar_endboss/blue/blue20.png',
-        '../img/7_statusbars/2_statusbar_endboss/blue/blue40.png',
-        '../img/7_statusbars/2_statusbar_endboss/blue/blue60.png',
-        '../img/7_statusbars/2_statusbar_endboss/blue/blue80.png',
-        '../img/7_statusbars/2_statusbar_endboss/blue/blue100.png',
+        '../img/7_statusbars/2_statusbar_endboss/green/green0.png',
+        '../img/7_statusbars/2_statusbar_endboss/green/green20.png',
+        '../img/7_statusbars/2_statusbar_endboss/green/green40.png',
+        '../img/7_statusbars/2_statusbar_endboss/green/green60.png',
+        '../img/7_statusbars/2_statusbar_endboss/green/green80.png',
+        '../img/7_statusbars/2_statusbar_endboss/green/green100.png',
     ];
 
     constructor() {
